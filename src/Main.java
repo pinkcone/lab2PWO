18f4ba5516e4be8e330c04e32fdc5dc35338851d
pinkcone <szymondudek4@gmail.com>
2024-10-17 13:51:11 +0200

    feat. homePage create

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        HomePage page = new HomePage("www.superstrona.pl");
+        page.print();
     }
 }
